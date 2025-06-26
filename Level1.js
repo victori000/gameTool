463b54e021f0b1400fafdc5fc894c0809fb82356
Victorio Gilistro <vitogilistro.90@gmail.com>
2025-06-26 14:35:51 -0300

    trying things

---
+++ Level1.js
@@ -0,0 +1,72 @@
+import Scene from './Scene.js';
+import Player from './Player.js';
+import NPC from './NPC.js';
+import Entity from './Entity.js';
+import Position from './Position.js';
+import Size from './Size.js';
+import SpriteAnimation from './SpriteAnimation.js';
+import StaticSprite from './StaticSprite.js';
+import Collider from './Collider.js';
+import Collision from './Collision.js';
+import DialogBox from './DialogBox.js';
+import Input from './Input.js';
+
+const frameWidth = 64;
+const frameHeight = 64;
+const frameCount = 1;
+const frameRate = 10;
+
+class Level1 extends Scene {
+    constructor(images) {
+        super('Level1');
+        // Entities
+        this.player = new Player(new Position(0, 0), new SpriteAnimation(images.player, frameWidth, frameHeight, frameCount, frameRate));
+        this.npc = new NPC(new Position(100, 100), new SpriteAnimation(images.npc, frameWidth, frameHeight, frameCount, frameRate));
+        this.door = new Entity();
+        this.doorPosition = new Position(200, 200);
+        this.doorSize = new Size(32, 32);
+        this.door.addComponent('position', this.doorPosition);
+        this.door.addComponent('size', this.doorSize);
+        this.door.addComponent('collider', new Collider(this.doorPosition, this.doorSize));
+        this.door.addComponent('sprite', new StaticSprite(images.door, this.doorSize.width, this.doorSize.height));
+
+        // Components
+        this.player.addComponent('size', new Size(frameWidth, frameHeight));
+        this.npc.addComponent('size', new Size(frameWidth, frameHeight));
+
+        // Dialog
+        this.dialogBox = new DialogBox();
+        this.dialogActive = false;
+    }
+
+    update(deltaTime) {
+        // Collision with NPC
+        const playerSize = this.player.getComponent('size');
+        const npcSize = this.npc.getComponent('size');
+        const collision = new Collision(this.player.position, playerSize, this.npc.position, npcSize);
+        if (collision.check()) {
+            if (!this.dialogActive && Input.isKeyPressed('e')) {
+                this.dialogBox.show('¡Hola! Soy el NPC.');
+                this.dialogActive = true;
+            }
+        }
+        if (this.dialogActive && Input.isKeyPressed('Escape')) {
+            this.dialogBox.hide();
+            this.dialogActive = false;
+        }
+
+        this.player.update(deltaTime);
+        this.npc.update(deltaTime);
+    }
+
+    draw(ctx) {
+        this.player.draw(ctx);
+        this.npc.draw(ctx);
+        const doorSprite = this.door.getComponent('sprite');
+        const doorPosition = this.door.getComponent('position');
+        doorSprite.draw(ctx, doorPosition.x, doorPosition.y);
+        this.dialogBox.draw(ctx);
+    }
+}
+
+export default Level1;
---
+++ MainMenu.js
@@ -0,0 +1,92 @@
+import Scene from './Scene.js';
+import Position from './Position.js';
+import Size from './Size.js';
+import Entity from './Entity.js';
+import StaticSprite from './StaticSprite.js';
+import DialogBox from './DialogBox.js';
+import Input from './Input.js';
+import loadImages from './loadImages.js';
+import Level1 from './Level1.js';
+
+
+class MainMenu extends Scene {
+    constructor(sceneManager, canvas, images) {
+        super('MainMenu');
+        this.sceneManager = sceneManager;
+        this.canvas = canvas;
+        // Background color
+        this.backgroundColor = 'black';
+
+        // Title
+        this.title = 'Mi Juego';
+        this.titlePosition = new Position(canvas.width / 2, canvas.height / 4);
+
+        // Start button
+        this.startButton = new Entity();
+        this.startButtonPosition = new Position(canvas.width / 2 - 50, canvas.height / 2);
+        this.startButtonSize = new Size(100, 50);
+        this.startButton.addComponent('position', this.startButtonPosition);
+        this.startButton.addComponent('size', this.startButtonSize);
+        this.startButton.addComponent(
+            'sprite',
+            new StaticSprite(images && images.button ? images.button : null, this.startButtonSize.width, this.startButtonSize.height)
+        );
+
+        // Components
+        this.startButton.addComponent('hover', false);
+
+        // Dialog
+        this.dialogBox = new DialogBox();
+        this.dialogActive = false;
+    }
+
+    update(deltaTime) {
+        // Check if the mouse is over the start button
+        const mousePosition = Input.getMousePosition();
+        const buttonSize = this.startButton.getComponent('size');
+        const buttonPosition = this.startButton.getComponent('position');
+        const hover = mousePosition.x >= buttonPosition.x && mousePosition.x <= buttonPosition.x + buttonSize.width &&
+            mousePosition.y >= buttonPosition.y && mousePosition.y <= buttonPosition.y + buttonSize.height;
+        this.startButton.addComponent('hover', hover);
+
+        // Start the game when clicking the start button
+        if (hover && Input.isMouseLeftPressed && Input.isMouseLeftPressed()) {
+            this.startGame();
+        }
+    }
+
+    draw(ctx) {
+        // Draw background
+        ctx.fillStyle = this.backgroundColor;
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Draw title
+        ctx.fillStyle = 'white';
+        ctx.font = '40px Arial';
+        ctx.fillText(this.title, this.titlePosition.x, this.titlePosition.y);
+
+        // Draw start button
+        const buttonSprite = this.startButton.getComponent('sprite');
+        const buttonPosition = this.startButton.getComponent('position');
+        buttonSprite.draw(ctx, buttonPosition.x, buttonPosition.y);
+
+        // Draw dialog box
+        this.dialogBox.draw(ctx);
+    }
+
+    startGame() {
+        const paths = {
+            player: 'assets/sprites/pj_especical.png',
+            npc: 'assets/sprites/pj_animated.png',
+            door: 'assets/sprites/door.png',
+        };
+        loadImages(paths).then(images => {
+            const level1 = new Level1(images);
+            this.sceneManager.changeScene(level1);
+        }).catch(error => {
+            console.error('Error loading images:', error);
+        });
+    }
+}
+
+export default MainMenu;
